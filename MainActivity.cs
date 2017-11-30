7d032242fe3d852ad3a03bb0430d0bd7eca30271
benpennywmc <33721273+benpennywmc@users.noreply.github.com>
2017-11-30 13:49:34 +0000

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using GR.Net.Maroulis.Library;
+using static Android.Resource;
+using Android.Views;
+
+namespace StepSplashScreen
+{
+    [Activity(Label = "StepSplashScreen", MainLauncher = true, Icon = "@drawable/icon",Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //Show Splash Screen
+            var config = new EasySplashScreen(this)
+                .WithFullScreen()
+                .WithTargetActivity(Java.Lang.Class.FromType(typeof(TargetActivity)))
+                .WithSplashTimeOut(5000) // 5 sec
+                .WithBackgroundColor(Android.Graphics.Color.ParseColor("#ffdfe0"))
+                .WithLogo(Resource.Drawable.step_logo);
+            //Set Text Color
+            //config.HeaderTextView.SetTextColor("#b85250");
+            //config.FooterTextView.SetTextColor("#b85250");
+            //config.BeforeLogoTextView.SetTextColor("#b85250");
+
+            //Create view
+            View view = config.Create();
+
+            //Set Content View 
+            SetContentView(view);
+        }
+    }
+}
+
---
+++ TargetActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+
+namespace StepSplashScreen
+{
+    [Activity(Label = "TargetActivity", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class TargetActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Target);
+        }
+    }
+}
